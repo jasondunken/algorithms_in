b107d09d3b884d0c1c9d2d197d97bebccdf64b14
jasondunken <jasondunken@yahoo.com>
2023-11-04 16:47:37 -0400

    add js binary search

---
+++ js/binary_search.js
@@ -0,0 +1,24 @@
+function binary_search(array, query) {
+    let l = 0;
+    let r = array.length - 1;
+
+    let i = Math.floor(l + (r - l) / 2);
+    while (l <= r) {
+        if (array[i] === query) return true;
+        if (array[i] < query) l = i + 1;
+        if (array[i] > query) r = i - 1;
+        i = Math.floor(l + (r - l) / 2);
+    }
+    return false;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log("binary_search testing...");
+let result = binary_search(arr, 5);
+console.log("result 1: ", result);
+console.assert(result === true, "%o", { error: "array does contain 5" });
+
+result = binary_search(arr, 10);
+console.log("result 2: ", result);
+console.assert(result === false, "%o", { error: "array does not contain 10" });
