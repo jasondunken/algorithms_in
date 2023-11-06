1e65e1ef4e9c000ad24b976c89213d69361509fc
jasondunken <jasondunken@yahoo.com>
2023-11-06 09:22:23 -0500

    add binary search in C#

---
+++ cs/algorithms_in/Program.cs
@@ -0,0 +1,10 @@
+﻿using algorithms_in;
+
+Console.WriteLine("testing binary search in C#...");
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+bool result = BinarySearch.binarySearch(array, 5);
+Console.WriteLine("result 1: " + result);
+
+result = BinarySearch.binarySearch(array, 10);
+Console.WriteLine("result 2: " + result);
