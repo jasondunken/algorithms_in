66d2dfa8043e28d33eccc4390e58cfc326fd4b07
jasondunken <jasondunken@yahoo.com>
2023-11-07 09:05:08 -0500

    add binary search in c

---
+++ c/binary_search.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef enum { false, true } bool;
+
+bool binary_search(int *, int);
+
+int main() {
+    printf("testing binary search in c...\n");
+    int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    bool result = binary_search(array, 5);
+    printf("result 1: %s -> expected true\n", result ? "true" : "false");
+
+    result = binary_search(array, 10);
+    printf("result 2: %s -> expected false\n", result ? "true" : "false");
+
+    return 0;
+}
+
+bool binary_search(int *array, int query) {
+    int l = 0;
+    int r = sizeof(array) - 1;
+
+    int i = l + (r - l) / 2;
+
+    while (l <= r) {
+        if (array[i] == query) return true;
+        if (array[i] < query) l = i + 1;
+        if (array[i] > query) r = i - 1;
+        i = l + (r - l) / 2;
+    }
+    return false;
+}
