1ef3610e7afcb5728f9b3276455984320a2092a6
jasondunken <jasondunken@yahoo.com>
2023-11-08 09:22:32 -0500

    add binary search in c++

---
+++ cpp/binary_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+bool binary_search(int[], int, int);
+
+void main() {
+    std::cout << "testing binary search in c++...";
+
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int size = sizeof(array) / sizeof(array[0]);
+
+    bool result = binary_search(array, size, 5);
+    string result_1_string = result ? "true" : "false";
+    std::cout << "result 1: " << string_1_result << " -> expected true";
+
+    result = binary_search(array, size, 10);
+    string result_2_string = result ? "true" : "false";
+    std::cout << "result 2: " << result_2_string << " -> expected false";
+
+    return 0;
+}
+
+bool binary_search(int array[], int size, int query) {
+    int l = 0;
+    int r = size - 1;
+
+    int i = l + (r - l) / 2;
+
+    while (l <= r) {
+        if (array[i] == query) return true;
+        if (array[i] < query) l = i + 1;
+        if (array[i] > query) r = i - 1;
+        i = l + (r - l) / 2;
+    }
+    return false;
+}
