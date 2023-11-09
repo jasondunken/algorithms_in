274160533a50e0298c20c1ec3bef262b6c29dd06
jasondunken <jasondunken@yahoo.com>
2023-11-09 13:37:23 -0500

    add recursive binary search in python

---
+++ python/recursive_binary_search.py
@@ -0,0 +1,23 @@
+def recursive_binary_search(array, query):
+    if len(array) == 0:
+        return False
+    
+    i = len(array) // 2
+    if array[i] == query:
+        return True
+    if array[i] < query:
+        return recursive_binary_search(array[i + 1:], query)
+    if array[i] > query:
+        return recursive_binary_search(array[:i], query)
+
+
+print("testing recursive binary search in python...")
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+result = recursive_binary_search(arr, 5)
+print("result 1: " + str(result))
+assert result == True
+
+result = recursive_binary_search(arr, 10)
+print("result 2: " + str(result))
+assert result == False
