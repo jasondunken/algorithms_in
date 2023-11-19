ff599774b1d62dc540475897e29a112b607916b8
jasondunken <jasondunken@yahoo.com>
2023-11-19 13:19:15 -0500

    add recursive binary search in c

---
+++ c/recursive_binary_search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef enum { false, true } bool;
+
+int main() {
+    printf("testing recursive binary search in c...\n");
+
+    int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    bool result = recursiveBinarySearch(array, 0, 8, 5);
+    printf("result 1: %s\n", result ? "true" : "false");
+
+    result = recursiveBinarySearch(array, 0, 8, 10);
+    printf("result 2: %s\n", result ? "true" : "false");
+}
+
+bool recursiveBinarySearch(int *array, int l, int r, int query) {
+    if (l >= r) return false;
+
+    int i = l + (r - l) / 2;
+
+    if (array[i] == query) return true;
+    if (array[i] < query) {
+        return recursiveBinarySearch(array, l + 1, r, query);
+    }
+    if (array[i] > query) {
+        return recursiveBinarySearch(array, l, r - 1, query);
+    }
+
+    return false;
+}
