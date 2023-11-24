610ba5c11c64e7d5715c4d5639d05b93233054cf
jasondunken <jasondunken@yahoo.com>
2023-11-24 09:00:58 -0500

    add selection sort in C#

---
+++ cs/algorithms_in/selection_sort.cs
@@ -0,0 +1,23 @@
+﻿namespace algorithms_in;
+
+public class SelectionSort
+{
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.Length; i++) {
+            int currentMin = array[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < array.Length; j++) {
+                if (currentMin > array[j]) {
+                    currentMin = array[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                array[currentMinIndex] = array[i];
+                array[i] = currentMin;
+            }
+        }
+    }
+}
