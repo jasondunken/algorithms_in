6ebd3fd320f04143526685c561ee8e804c6e0b68
jasondunken <jasondunken@yahoo.com>
2023-11-25 13:41:09 -0500

    add selection sort in c

---
+++ c/selection_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void selection_sort(int *array, int size);
+
+int main() {
+    int array[] = { 9, 5, 7, 1, 6, 2, 3, 8, 4 };
+    int size = sizeof(array) / sizeof(int);
+
+    selection_sort(array, size);
+
+    printf("sorted array -> [ %d", array[0]);
+    for (int i = 1; i < size; i++) {
+        printf(", %d", array[i]);
+    }
+    printf(" ]\n");
+
+    return 0;
+}
+
+void selection_sort(int array, int size) {
+    for (i = 0; i < size; i++) {
+        int currentMin = array[i];
+        int currentMinIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (currentMin > array[j]) {
+                currentMin = array[j];
+                currentMinIndex = j;
+            }
+        }
+        if (currentMinIndex != i) {
+            array[currentMinIndex] = array[i];
+            array[i] = currentMin;
+        }
+    }
+}
