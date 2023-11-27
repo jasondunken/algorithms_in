41302bed1abf20f9b31ff216a75a582ada447bb1
jasondunken <jasondunken@yahoo.com>
2023-11-27 18:40:50 -0500

    add rust logging

--- rust/algorithms_in/src/main.rs
+++ rust/algorithms_in/src/main.rs
@@ -20,6 +20,8 @@ fn main() {
 
     result = recursive_binary_search::recursive_binary_search(&array, 0, 8, 10);
     println!("result 4: {} -> expected false", result);
+
+    println!("testing selection sort in rust...");
     
     let mut unsorted_array: [usize; 9] = [9, 5, 7, 1, 6, 2, 3, 8, 4];
     selection_sort::selection_sort(&mut unsorted_array);
