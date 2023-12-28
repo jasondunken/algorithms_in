6b164940d2ab633a1d84b41e0a0f52bf8b725c5e
jasondunken <jasondunken@yahoo.com>
2023-12-27 21:06:04 -0500

    add heap sort in java

---
+++ javalang/heapsort/HeapSort.java
@@ -0,0 +1,23 @@
+package javalang.heapsort;
+
+public class HeapSort {
+    public static <E extends Comparable> void heapSort(E[] list) {
+        Heap<E> heap = new Heap<E>();
+
+        for (int i = 0; i < list.length; i++) {
+            heap.add(list[i]);
+        }
+
+        for (int i = list.length - 1; i >= 0; i--) {
+            list[i] = heap.remove();
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = { 2, 3, 2, 5, 6, 1, -2, 3, 14, 12 };
+        heapSort(list);
+        for (int i = 0; i < list.length; i++ ) {
+            System.out.print(list[i] + " ");
+        }
+    }
+}
