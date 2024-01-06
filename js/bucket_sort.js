ec16096a297658ae05dd6ab7b8bda1b14b7ebf3e
jasondunken <jasondunken@yahoo.com>
2024-01-06 17:13:49 -0500

    add bucket sort with negative numbers in js

--- js/bucket_sort.js
+++ js/bucket_sort.js
@@ -1,3 +1,4 @@
+// only works with non-negative numbers
 function bucketSort(array) {
     let n = Number.MIN_VALUE;
     for (let i = 0; i < array.length; i++) {
@@ -28,6 +29,48 @@ function bucketSort(array) {
     }
 }
 
+// works with negative numbers
+function bucketSort2(array) {
+    let min = Number.MAX_VALUE;
+    let max = Number.MIN_VALUE;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    const range = max - min;
+    console.log("range: ", range);
+    const offset = 0 - min;
+    console.log("offset: ", offset);
+
+    const buckets = new Array(range).fill(null);
+    console.log("buckets: ", buckets);
+    for (let i = 0; i < array.length; i++) {
+        const key = array[i] + offset;
+        console.log("key: ", key);
+        if (buckets[key] == null) {
+            buckets[key] = [];
+        }
+        buckets[key].push(array[i]);
+    }
+
+    let k = 0;
+    for (let i = 0; i < buckets.length; i++) {
+        if (buckets[i] != null) {
+            const bucket = buckets[i];
+            for (let j = 0; j < bucket.length; j++) {
+                array[k++] = bucket[j];
+            }
+        }
+    }
+}
+
 function testBucketSort() {
     output.innerHTML += "<h3>testing bucket sort in javascript...</h3>";
     const unsortedArray = getRandArray(10, 1);
@@ -36,4 +79,13 @@ function testBucketSort() {
     output.innerHTML += `<p>sorted array -> [${unsortedArray}]</p>`;
 }
 
+function testBucketSort2() {
+    output.innerHTML += "<h3>testing bucket sort 2 in javascript...</h3>";
+    const unsortedArray = getRandArray(10, -2);
+    output.innerHTML += `<p>unsorted array -> [${unsortedArray}]</p>`;
+    bucketSort2(unsortedArray);
+    output.innerHTML += `<p>sorted array -> [${unsortedArray}]</p>`;
+}
+
 testBucketSort();
+testBucketSort2();
