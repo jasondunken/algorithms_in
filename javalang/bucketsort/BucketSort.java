8b2dc4526a6ca85bd8685496268d619065d8b499
jasondunken <jasondunken@yahoo.com>
2024-01-08 08:02:51 -0500

    add bucket search with negative numbers in java

--- javalang/bucketsort/BucketSort.java
+++ javalang/bucketsort/BucketSort.java
@@ -34,12 +34,48 @@ public class BucketSort {
         }
     }
 
+    public static void bucketSort2(int[] list) {
+        int min = list[0];
+        int max = list[0];
+        for (int i = 1; i < list.length; i++) {
+            if (list[i] < min) {
+                min = list[i];
+            }
+            if (list[i] > max) {
+                max = list[i];
+            }
+        }
+
+        int range = max - min + 1;
+        int offset = 0 - min;
+
+        ArrayList<Integer>[] buckets = new ArrayList[range];
+
+        for (int i = 0; i < list.length; i++) {
+            int key = list[i] + offset;
+            if (buckets[key] == null) {
+                buckets[key] = new ArrayList<Integer>();
+            }
+            buckets[key].add(list[i]);
+        }
+
+        int k = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] != null) {
+                ArrayList<Integer> bucket = buckets[i];
+                for (int j = 0; j < bucket.size(); j++) {
+                    list[k++] = (int) bucket.get(j);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("testing bucket sort in java...");
-        int[] unsortedArray = { 9, 5, 7, 1, 6, 2, 5, 7, 1, 6, 2, 3, 8, 4 };
+        int[] unsortedArray = { 9, 5, 7, 1, -6, 2, 5, 7, -1, 6, 2, 3, 8, 4 };
 
         System.out.println("unsorted array: " + Arrays.toString(unsortedArray));
-        bucketSort(unsortedArray);
+        bucketSort2(unsortedArray);
         System.out.println("sorted array: " + Arrays.toString(unsortedArray));
     }
 }
