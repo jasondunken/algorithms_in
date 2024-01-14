222c47ad74c9a31e825863a69fb67f8adc2a7002
jasondunken <jasondunken@yahoo.com>
2024-01-14 08:25:55 -0500

    add bucket sort variant in c#

--- cs/algorithms_in/bucket_sort.cs
+++ cs/algorithms_in/bucket_sort.cs
@@ -31,4 +31,36 @@ public class BucketSort
             }
         }
     }
+
+    public static void bucketSort2(int[] list) {
+        int min = list[0];
+        int max = list[0];
+        for (int i = 1; i < list.Length; i++) {
+            if (list[i] < min) {
+                min = list[i];
+            }
+            if (list[i] > max) {
+                max = list[i];
+            }
+        }
+
+        int range = max - min + 1;
+        int offset = 0 - min;
+
+        int[] buckets = new int[range];
+
+        for (int i = 0; i < list.Length; i++) {
+            int key = list[i] + offset;
+            buckets[key] = buckets[key] + 1;
+        }
+
+        int k = 0;
+        for (int i = 0; i < buckets.Length; i++) {
+            if (buckets[i] > 0) {
+                for (int j = 0; j < buckets[i]; j++) {
+                    list[k++] = i - offset;
+                }
+            }
+        }
+    }
 }
